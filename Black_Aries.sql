6a385c2dd61f10956f20750e08119b8ac2a0b466
binhhoang249 <hoangthanhbinh24092005@gmail.com>
2024-12-11 11:37:16 +0700

    8 | Done insert data to table

--- Black_Aries.sql
+++ Black_Aries.sql
@@ -65,6 +65,30 @@ INSERT INTO Product_color (quantity, image, price, defaultal, product_id, color_
     (8, 'queen_bed_black.png', 500, 1, 4, 4),
     (20, 'desk_lamp_white.png', 50, 1, 5, 5);
 
+-- Nếu ai đã chạy hết tất cả database rồi thì chạy thêm lệnh này nữa, chạy lần lượt tránh lỗi.
+ALTER TABLE Products
+    MODIFY description TEXT;
+
+UPDATE Products
+SET description = 'Modern dining table with a sleek design, crafted from high-quality wood, offering durability and elegance for any dining space. The table features a spacious surface, allowing for comfortable dining experiences with family and friends. Its minimalist aesthetic blends seamlessly with modern interiors, while the sturdy legs provide stability and long-lasting support.'
+WHERE product_id = 1;
+
+UPDATE Products
+SET description = 'Comfortable corner sofa with plush cushions and a sturdy frame, providing ample seating space for family and guests. This sofa offers a contemporary design with soft fabric upholstery, making it a cozy spot for relaxation. The L-shaped configuration is perfect for maximizing seating in small living rooms, and its durable build ensures longevity for years to come.'
+WHERE product_id = 2;
+
+UPDATE Products
+SET description = 'Ergonomic office chair designed for maximum comfort and support, featuring adjustable height, lumbar support, and breathable mesh material. The chair is equipped with 360-degree swivel functionality, smooth-rolling casters, and a recline feature to suit different work positions. Its sleek, modern look makes it a perfect fit for any home office or workspace.'
+WHERE product_id = 3;
+
+UPDATE Products
+SET description = 'Stylish queen-size bed with a modern headboard, crafted with high-quality materials to ensure a comfortable and restful night’s sleep. The bed frame is designed to support various mattress types, providing versatility for different preferences. Its sophisticated design enhances the bedroom’s decor, while the sturdy construction guarantees lasting durability.'
+WHERE product_id = 4;
+
+UPDATE Products
+SET description = 'LED desk lamp with adjustable brightness and a flexible neck, perfect for focused lighting while studying or working. The lamp’s energy-efficient LED bulbs provide bright, long-lasting illumination. It features a touch-sensitive control panel for easy adjustments and a modern design that complements any workspace or study area.'
+WHERE product_id = 5;
+
 
 
 
