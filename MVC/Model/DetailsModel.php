0de914d10b970c3d1d7d43e6cf8ed47c2fade932
Hiinn3108 <voh27651@gmail.com>
2024-12-12 16:26:25 +0700

    update

--- MVC/Model/DetailsModel.php
+++ MVC/Model/DetailsModel.php
@@ -1,25 +1,42 @@
 <?php
-    class DetailsModel  extends DModel {
-        public function __construct() {
-            parent::__construct();
-        }
-        public function getProductDetails($id) {
-            $sql = "SELECT 
-                        products.name, 
-                        products.description, 
-                        GROUP_CONCAT(product_color.price) AS prices
-                    FROM 
-                        products
-                    INNER JOIN 
-                        product_color 
-                    ON 
-                        products.id = product_color.product_id
-                    WHERE 
-                        products.id = ?
-                    GROUP BY 
-                        products.id";
-            return $this->db->select($sql, [$id]);
-        }
-        
+class DetailsModel extends DModel {
+    public function __construct() {
+        parent::__construct();
     }
+    public function getProductDetails($id) {
+        $sql = "SELECT 
+                    products.name, 
+                    products.description, 
+                    product_color.images AS image, 
+                    product_color.price AS price
+                FROM 
+                    products
+                INNER JOIN 
+                    product_color 
+                ON 
+                    products.id = product_color.product_id
+                WHERE 
+                    products.id = ? 
+                    AND product_color.is_default = 1
+                LIMIT 1";
+        return $this->db->select($sql, [$id]);
+    }
+
+    public function getsliderimages($id) {
+        $sql = "SELECT 
+                    product_color.images AS image 
+                FROM 
+                    product_color
+                INNER JOIN 
+                    products ON products.id = product_color.product_id
+                WHERE 
+                    products.id = ?
+                    AND product_color.is_default = 0";
+        return $this->db->select($sql, [$id]);
+    }
+    
+}
+
+
 ?>
+
