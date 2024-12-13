5c2a2b56944b4db9acb7dcb021a4836bf9554a07
binhhoang249 <hoangthanhbinh24092005@gmail.com>
2024-12-13 14:35:30 +0700

    Done abouts us

--- MVC/Views/AboutUsPage.php
+++ MVC/Views/AboutUsPage.php
@@ -1,23 +1,27 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+
+    <!-- Đảm bảo đường dẫn CSS đúng -->
     <link rel="stylesheet" type="text/css" href="http://localhost/Black-Aries-Project/public/css/AboutUs.css">
-    <title>About us</title>
 </head>
+
 <body>
     <div class="container">
-        <?php extract($data);
-        include_once 'MVC/Views/header.php';
-        ?>
+        <?php include_once 'MVC/Views/header.php'; ?>
+
         <div class="banner">
             <img src="http://localhost/Black-Aries-Project/public/images/aboutus.png" alt="Banner">
             <h1 class="title">About Us</h1>
         </div>
-        <div>
-        <img src="http://localhost/Black-Aries-Project/public/images/Air_Bed_black.png" alt="ádad">
+        <div class="information-business">
+            <h2><?php echo htmlspecialchars($data['businessName']); ?></h2>
+            <p><?php echo nl2br(htmlspecialchars($data['description'])); ?></p>
         </div>
     </div>
 </body>
