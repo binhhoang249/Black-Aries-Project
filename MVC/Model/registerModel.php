d962236cef960a6f7c5052eecdf2332cdb66d567
binhhoang249 <hoangthanhbinh24092005@gmail.com>
2024-12-17 09:46:15 +0700

    Done add processing function to model file

--- MVC/Model/registerModel.php
+++ MVC/Model/registerModel.php
@@ -3,5 +3,21 @@ class RegisterModel extends DModel {
     public function __construct() {
         parent::__construct();
     }
+    public function addUser($name, $email, $password) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $data = [
+            'full_name' => $name,
+            'email' => $email,
+            'password' => $hashedPassword
+        ];
+        $checkSql = "SELECT * FROM users WHERE email = :email";
+        $checkUser = $this->db->select($checkSql, ['email' => $email]);
+
+        if (!empty($checkUser)) {
+            return ['status' => false, 'message' => 'Email đã tồn tại!'];
+        }
+        $result = $this->db->insert("users", $data);
+        return $result ? ['status' => true, 'message' => 'Đăng ký thành công!'] : ['status' => false, 'message' => 'Lỗi khi thêm người dùng!'];
+    }
 }
 ?>
