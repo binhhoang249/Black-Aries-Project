7e54071e629454b4e54c11938d05d22eca23036e
binhhoang249 <hoangthanhbinh24092005@gmail.com>
2025-01-15 00:07:02 -0800

    Done pagination for order mamagement page

--- MVC/core/App.php
+++ MVC/core/App.php
@@ -1,24 +1,30 @@
 <?php
     class App {
+        // http://localhost/BlackArirsProjecct/Home/Sayhi/12/3123
         protected $controller = "HomeController";
         protected $action = "index";
         protected $params;
         function __construct() {
            $arr = $this->UrlProcess();
+        // Xu li controller, no se goi controller nao, cu the hon la page nao
+        if(isset($arr[0])){
             if( file_exists("./MVC/controllers/" . $arr[0] . ".php") ) {
-                $this->controller = $arr[0];   
-                unset($arr[0]);
+                $this->controller = $arr[0];  
+            } 
+            unset($arr[0]);
+        }
+        require_once "./MVC/controllers/".$this->controller.".php";
+        $this->controller = new $this->controller;
+        // Xu li fuction, no se goi thang function nao-Action
+        if(isset($arr[1])  ) {
+            if(method_exists($this->controller, $arr[1])) {
+                $this->action = $arr[1];
             }
-            require_once "./MVC/controllers/".$this->controller.".php";
-            $this->controller = new $this->controller;
-            if(isset($arr[1])  ) {
-                if(method_exists($this->controller, $arr[1])) {
-                    $this->action = $arr[1];
-                }
-                unset($arr[1]);
-            }   
-            $this->params = $arr?array_values($arr):[];
-            call_user_func_array([$this->controller, $this->action], $this->params);
+            unset($arr[1]);
+        }
+        // Xu li params
+        $this->params = $arr?array_values($arr):[];
+        call_user_func_array([$this->controller, $this->action], $this->params);
         }
         
         function UrlProcess () {
@@ -28,3 +34,4 @@
         }
     }
 ?>
+<!-- Muốn chuyển trang mới thì tạo page tương ứng trong controller -->
